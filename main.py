6e66a8f2b84fdafb2257f1b20459ec60513ae4e4
kanokkorn <kanokorn@outlook.jp>
2023-02-12 22:54:23 +0700

    ported from old project

--- main.py
+++ main.py
@@ -37,6 +37,43 @@ def count():
         if not ret:
             # throw error and require reboot
             break
+        gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 2)
+        #blur = cv2.bilateralFilter(blur, 9, 15, 15)
+        gray = back_sub.apply(cv2.GaussianBlur(gray, (5, 5), 2))
+        canny = cv2.Canny(gray, 10, 20)
+        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        contours, hierarachy = cv2.findContours(thresh, cv2.RETR_TREE,
+        for c in contours:
+            print(len(contours))
+            if len(contours) > 1: cv2.waitKey(-1)
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            area = cv2.contourArea(c)
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+            cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
+            cv2.putText(img, f'[Development]', (50, 50), cv2.FONT_HERSHEY_DUPLEX, 1,
+                        (0, 255, 0), 1)
+            try:
+                curr_val = len(contours)
+                delta += curr_val - prev_val
+                total += delta
+                prev_val = curr_val
+                cv2.putText(img, f'computer see: {total//4}', (50, 100),
+                            cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 1)
+            except TypeError:
+                cv2.putText(img, f'computer see: {total//4}', (50, 100),
+                            cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 1)
+                cv2.imshow('out', img)
+                if cv2.waitKey(1) == ord('r'):
+                    curr_val = prev_val = delta = total = 0
+                elif cv2.waitKey(1) == ord('q'):
+                    cv2.GaussianBlur(gray, (5, 5), 2)
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    break
 
 if __name__ == '__main__':
     check_platform()
