347ab64d2c17bb267a659f537ddd6ad7da19f6a4
kanokkorn <kanokorn@outlook.jp>
2023-10-02 23:54:50 +0700

    change from direct connect to i2c

--- hw_ctrl.py
+++ hw_ctrl.py
@@ -1,23 +1,21 @@
 try:
     import RPi.GPIO as GPIO
+    import smbus
 except RuntimeError:
     print("privileges error, try with superuser")
 
+# 16x2 LCD with i2c protocol
+DEVICE_ADDR = 0x3f
+DEVICE_BUS = 1
+bus = smbus.SMBus(DEVICE_BUS)
+
+# RPi GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarning(False)
 GPIO.setup()
-
-# Define GPIO to LCD mapping
-LCD_RS = 7
-LCD_E = 8
-LCD_D4 = 25
-LCD_D5 = 24
-LCD_D6 = 23
-LCD_D7 = 18
 
 
 # flush and display to LCD
 def display_lcd(input_data):
     display_text = str(input_data)
-    print(display_text)
-    pass
+    bus.write_byte_data(DEVICE_ADDR, display_text, 0x01)
